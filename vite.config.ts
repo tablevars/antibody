6e48627cbf9df4eeeeb4cf84be7fffd616aa3f86
lilaw <takialog@gmail.com>
2024-11-11 11:22:29 +0800

    change build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
